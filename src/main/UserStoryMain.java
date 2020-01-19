503ecc510fc9324af49237464b2e02f489691725
Sowmya <Sowmya@10.0.0.15>
2020-01-19 11:17:05 +1100

    testing commit

---
+++ src/main/UserStoryMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Sowmya
+ *
+ */
+public class UserStoryMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Entering Main()");
+		
+		System.out.println("Exitting Main()");
+
+	}
+
+}
