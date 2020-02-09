11cca9f6515aaf441b623d5b24a0223cc06a2dd0
Sowmya <Sowmya@10.0.0.15>
2020-02-09 14:54:53 +1100

    UserControllerTest

--- UserStory/src/test/java/com/archimydes/userstory/controller/UserControllerTest.java
+++ UserStory/src/test/java/com/archimydes/userstory/controller/UserControllerTest.java
@@ -1,51 +1,31 @@
-/**
- * 
- */
 package com.archimydes.userstory.controller;
-
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.archimydes.userstory.domain.User;
 import com.archimydes.userstory.domain.UserRoles;
-import com.archimydes.userstory.domain.UserStory;
 import com.archimydes.userstory.domain.types.RoleType;
-import com.archimydes.userstory.repository.UserRepository;
-import com.nimbusds.oauth2.sdk.http.HTTPRequest;
 
 /**
  * @author Sowmya
  *
  */
 public class UserControllerTest {
-	/*
-	 * @Autowired private UserRepository repository;
-	 * 
-	 * @Autowired private UserController userController;
-	 */
 	
 	 User reqUser = new User();
 	 User reqAdminUser = new User();
 	 User reqNonExistingUser = new User();
-	 UserRoles roles = new UserRoles();
-	 Set<UserRoles> roleSet = new HashSet<>();
+	 User reqInvalidUser = new User();
 	
 	@Before
 	public void setUp() {		
@@ -58,10 +38,9 @@ public class UserControllerTest {
 		    reqNonExistingUser.setName("testuser22");
 		    reqNonExistingUser.setPassword("bbb");
 		    
-		    roles.setId(2);
-		    roles.setName(RoleType.ROLE_USER);
-		   
-		    roleSet.add(roles);
+		    reqInvalidUser.setName("testuser2");
+		    reqInvalidUser.setPassword("aaa");
+		    
 	}
 	
 	@Test
@@ -78,9 +57,9 @@ public class UserControllerTest {
 	    //Verify request succeed
 	    System.out.println("result.getStatusCodeValue() -- "+result.getStatusCodeValue());
 	    Assert.assertEquals(200, result.getStatusCodeValue());
-	   // Assert.assertEquals(true, result.getBody());
-	    System.out.println("result.getBody().getId().intValue() -- "+result.getBody().getId().intValue());
-	    Assert.assertEquals(2, result.getBody().getId().intValue());
+	    Assert.assertEquals(true, reqUser.getPassword().equals(result.getBody().getPassword()));	    
+	    Assert.assertEquals(true,isRolePresent(result.getBody().getUserRoles(),RoleType.ROLE_USER));
+	    Assert.assertEquals(false,isRolePresent(result.getBody().getUserRoles(),RoleType.ROLE_ADMIN));
 	}
 	
 	@Test
@@ -97,102 +76,43 @@ public class UserControllerTest {
 		  //Verify request succeed
 		  System.out.println("result.getStatusCodeValue() -- "+result.getStatusCodeValue());
 		  Assert.assertEquals(200, result.getStatusCodeValue());
-		  System.out.println("result.getBody().getId().intValue() -- "+result.getBody().getId().intValue()); 
-		  Assert.assertEquals(1, result.getBody().getId().intValue());
+		//  Assert.assertEquals(true, result.getBody());
+		  Assert.assertEquals(true, reqAdminUser.getPassword().equals(result.getBody().getPassword()));
+		  Assert.assertEquals(true,isRolePresent(result.getBody().getUserRoles(),RoleType.ROLE_ADMIN));
 		 }
 	
-	@Test
-	public void testPostLoginUserHasUserRole() throws URISyntaxException 
-	{
-		/*
-		 * HttpEntity<User> request = new HttpEntity<>(reqUser);
-		 * 
-		 * RestTemplate restTemplate = new RestTemplate(); final String baseUrl =
-		 * "http://localhost:8080/api/login"; URI uri = new URI(baseUrl);
-		 * 
-		 * ResponseEntity<User> result = restTemplate.postForEntity(uri, request,
-		 * User.class);
-		 * 
-		 * //Verify request succeed
-		 * System.out.println("result.getStatusCodeValue() -- "+result.
-		 * getStatusCodeValue()); Assert.assertEquals(200, result.getStatusCodeValue());
-		 * // Assert.assertEquals(true, result.getBody());
-		 * System.out.println("result.getBody().getId().intValue() -- "+result.getBody()
-		 * .getId().intValue()); Assert.assertEquals(2,
-		 * result.getBody().getId().intValue());
-		 * System.out.println("result.getBody().getUserRoles() -- "+result.getBody().
-		 * getUserRoles().forEach(roles).); Assert.assertEquals(roleSet.,
-		 * result.getBody().getUserRoles().);
-		 */}	
-	
+	private boolean isRolePresent(Set<UserRoles> userRolesSet, RoleType roleAdmin) {
+	    boolean isRolePresent = false;
+	    for (UserRoles userRole : userRolesSet) {
+	    	System.out.println("userRole.getName(),roleAdmin  -- "+userRole.getName()+" , "+roleAdmin);
+	      isRolePresent = userRole.getName().equals(roleAdmin);
+	      if (isRolePresent) break;
+	    }
+	    System.out.println("isRolePresent -- "+isRolePresent);
+	    return isRolePresent;
+	  }
 	
 	
 	@Test
-	public void testAdminLoginUserHasUserRole() throws URISyntaxException 
+	public void testPostLoginInvalidCredentials() throws URISyntaxException 
 	{
-		/*
-		 * HttpEntity<User> request = new HttpEntity<>(reqUser);
-		 * 
-		 * RestTemplate restTemplate = new RestTemplate(); final String baseUrl =
-		 * "http://localhost:8080/api/login"; URI uri = new URI(baseUrl);
-		 * 
-		 * ResponseEntity<User> result = restTemplate.postForEntity(uri, request,
-		 * User.class);
-		 * 
-		 * //Verify request succeed
-		 * System.out.println("result.getStatusCodeValue() -- "+result.
-		 * getStatusCodeValue()); Assert.assertEquals(200, result.getStatusCodeValue());
-		 * // Assert.assertEquals(true, result.getBody());
-		 * System.out.println("result.getBody().getId().intValue() -- "+result.getBody()
-		 * .getId().intValue()); Assert.assertEquals(2,
-		 * result.getBody().getId().intValue());
-		 * System.out.println("result.getBody().getUserRoles() -- "+result.getBody().
-		 * getUserRoles().forEach(roles).); Assert.assertEquals(roleSet.,
-		 * result.getBody().getUserRoles().);
-		 */}	
-	@Test
-	public void testAdminLoginUserHasInsufficientRoles() throws URISyntaxException 
-	{
-		/*
-		 * HttpEntity<User> request = new HttpEntity<>(reqUser);
-		 * 
-		 * RestTemplate restTemplate = new RestTemplate(); final String baseUrl =
-		 * "http://localhost:8080/api/login"; URI uri = new URI(baseUrl);
-		 * 
-		 * ResponseEntity<User> result = restTemplate.postForEntity(uri, request,
-		 * User.class);
-		 * 
-		 * //Verify request succeed
-		 * System.out.println("result.getStatusCodeValue() -- "+result.
-		 * getStatusCodeValue()); Assert.assertEquals(200, result.getStatusCodeValue());
-		 * // Assert.assertEquals(true, result.getBody());
-		 * System.out.println("result.getBody().getId().intValue() -- "+result.getBody()
-		 * .getId().intValue()); Assert.assertEquals(2,
-		 * result.getBody().getId().intValue());
-		 * 
-		 */}
+		HttpEntity<User> request = new HttpEntity<>(reqInvalidUser);
+		
+		RestTemplate restTemplate = new RestTemplate();	     
+	    final String baseUrl = "http://localhost:8080/api/login";
+	    URI uri = new URI(baseUrl);      
+	    try
+	    {
+	    	restTemplate.postForEntity(uri, request, User.class);
+	    	Assert.fail();
+	    }	     
+	    catch(HttpClientErrorException ex) 
+	    {
+	       System.out.println("testAdminLoginUserDoesNotExist :: Exception -- ");
+	       Assert.assertEquals(404, ex.getRawStatusCode());
+	    }
+	}
 	
-	@Test
-	public void testUserLoginUserInvalidCredentials() throws URISyntaxException 
-	{
-		/*
-		 * HttpEntity<User> request = new HttpEntity<>(reqUser);
-		 * 
-		 * RestTemplate restTemplate = new RestTemplate(); final String baseUrl =
-		 * "http://localhost:8080/api/login"; URI uri = new URI(baseUrl);
-		 * 
-		 * ResponseEntity<User> result = restTemplate.postForEntity(uri, request,
-		 * User.class);
-		 * 
-		 * //Verify request succeed
-		 * System.out.println("result.getStatusCodeValue() -- "+result.
-		 * getStatusCodeValue()); Assert.assertEquals(200, result.getStatusCodeValue());
-		 * // Assert.assertEquals(true, result.getBody());
-		 * System.out.println("result.getBody().getId().intValue() -- "+result.getBody()
-		 * .getId().intValue()); Assert.assertEquals(2,
-		 * result.getBody().getId().intValue());
-		 * 
-		 */}
 	@Test
 	public void testAdminLoginUserDoesNotExist() throws URISyntaxException 
 	{
@@ -208,9 +128,10 @@ public class UserControllerTest {
 		  }
 		  catch(HttpClientErrorException ex) 
 		    {
-		       System.out.println("Exception -- ");
+		       System.out.println("testAdminLoginUserDoesNotExist :: Exception -- ");
 		       Assert.assertEquals(404, ex.getRawStatusCode());
 		    }
 		  
 		 }
+	
 }
